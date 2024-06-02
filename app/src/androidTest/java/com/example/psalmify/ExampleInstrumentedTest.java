ba0e06c76df4210e0041cfee6cdcab0076ba11ba
GrandPaPaRa <tomaghghgh@gmail.com>
2024-06-02 19:56:30 +0300

    fix google bug

--- app/src/androidTest/java/com/example/psalmify/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/psalmify/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.authenticationapp", appContext.getPackageName());
+        assertEquals("com.example.psalmify", appContext.getPackageName());
     }
 }
