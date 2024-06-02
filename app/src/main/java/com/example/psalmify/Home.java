d1a74d7825d7a7a7deea9f0c8b7a0dc9214ef739
David-UniTBv <secareanu.david2003@gmail.com>
2024-06-02 19:28:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/psalmify/Home.java
@@ -0,0 +1,25 @@
+package com.example.psalmify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+    public void login(View view){
+        startActivity(new Intent(getApplicationContext(),Login.class));
+        finish();
+    }
+
+    public void register(View view){
+        startActivity(new Intent(getApplicationContext(),Register.class));
+        finish();
+    }
+}
